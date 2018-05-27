6ea427883110cc3a87ab43c509c955565006780e
Carlos Cardoso Dias <carlosdias.dev@gmail.com>
2018-05-27 14:11:21 -0300

    Add test framework to project

---
+++ test/test.js
@@ -0,0 +1,12 @@
+const chai   = require('chai')
+const expect = chai.expect
+
+// Simple test for the framework
+describe('This is a simple integration test for the framework', () => {
+
+  it('should pass with a true boolean value', () => {
+    expect(true).to.equal(true)
+  })
+
+})
+
